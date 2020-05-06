6b098599d32d97e46adec9f9b38905bdfd811e83
Kevin Jin <kjin@cs.utexas.edu>
2020-05-06 17:07:24 -0500

    documentation

--- heap.c
+++ heap.c
@@ -9,26 +9,39 @@ volatile long mCount;
 volatile long fCount;
 
 
+// node in red black tree
 typedef struct rb_node {
 		struct rb_node *parent, *left, *right;
 } node;
 
+// container before and end of block of free space 
+// contains information about node for block
 typedef struct {
 		size_t size : 58;
 		int is_black : 3;
 		int is_used : 3;
 } meta;
 
+// pointer that holds the root of red black tree
+// only free blocks in red black tree
 node *temp = (node*) the_heap;
 node **free_list = &temp; 
 int has_init = 0;
 
-size_t get_size(node *cur_node);
-int get_is_black(node *cur_node);
-int get_is_used(node *cur_node);
-void set_size(node *cur_node, size_t size);
-void set_is_black(node *cur_node, int is_black);
-void set_is_used(node *cur_node, int is_used);
+// helper function that gets size
+size_t get_size(node *location)
+{
+    meta *cur_meta = (meta *)(((void *)location) - 8);
+    return cur_meta->size;
+}
+
+// helper function that sets size of headers
+void set_size(node *location, size_t size) {
+		meta *cur_meta = (meta*) (((void*) location) - 8);
+		meta *next_meta = (meta*) (((void*) location) + size);
+		cur_meta->size = size;
+		next_meta->size = size;
+}
 
 // helper function that gets is_black
 int get_is_black(node *location)
@@ -37,7 +50,7 @@ int get_is_black(node *location)
     return cur_meta->is_black;
 }
 
-// helper function that sets is_black
+// helper function that sets is_black of headers
 void set_is_black(node *location, int is_black) {
 		meta *cur_meta = (meta*) (((void*) location) - 8);
 		meta *next_meta = (meta*) (((void*) location) + get_size((node*) location));
@@ -52,7 +65,7 @@ int get_is_used(node *location)
     return cur_meta->is_used;
 }
 
-// helper function that sets is_used
+// helper function that sets is_used of headers
 void set_is_used(node *location, int is_used) {
 		meta *cur_meta = (meta*) (((void*) location) - 8);
 		meta *next_meta = (meta*) (((void*) location) + get_size((node*) location));
@@ -60,23 +73,23 @@ void set_is_used(node *location, int is_used) {
 		next_meta->is_used = is_used;
 }
 
-// helper function that gets size
-size_t get_size(node *location)
-{
-    meta *cur_meta = (meta *)(((void *)location) - 8);
-    return cur_meta->size;
+void make_node(void *location, node *left, node *right, node *parent) {
+		node *cur_loc = (node*) location;
+		node cur_node = {parent, left, right};
+		*cur_loc = cur_node;
 }
 
-// helper function that sets size
-void set_size(node *location, size_t size) {
-		meta *cur_meta = (meta*) (((void*) location) - 8);
-		meta *next_meta = (meta*) (((void*) location) + size);
-		cur_meta->size = size;
-		next_meta->size = size;
+void make_meta(void *location, size_t size, int is_black, int is_used) { 
+		meta *cur_loc = (meta*) location;
+		meta cur_meta = {size, is_black, is_used};
+		*cur_loc = cur_meta;
 }
+
+
+// ~~~~~~~~ INSERTION BEGINS HERE ~~~~~~~~~~
 
 // helper function that compares two nodes
-// returns
+// returns if orig > new 
 int compare(node *orig, node *new)
 {
     size_t orig_size = get_size(orig);
@@ -89,6 +102,7 @@ int compare(node *orig, node *new)
         return new_size > orig_size;
 }
 
+// performs left rotation around ptr with a base of root
 void left_rotate(node **root, node *ptr)
 {
     // right child takes current node's place
@@ -124,6 +138,7 @@ void left_rotate(node **root, node *ptr)
     }
 }
 
+// performs right rotation around ptr with a base of root
 void right_rotate(node **root, node *ptr)
 {
     node *new_root = ptr->left;
@@ -154,8 +169,10 @@ void right_rotate(node **root, node *ptr)
 // function that fixes the insertion of cur_node from the bottom up
 void fix(node **root, node *cur_node)
 {
+	// propagate upwards to check
     while (cur_node != *root && !get_is_black(cur_node->parent))
     {
+		// getting the uncle
         node *uncle = (cur_node->parent == cur_node->parent->parent->left) ? cur_node->parent->parent->right : cur_node->parent->parent->left;
 
         // if uncle is red
@@ -217,6 +234,7 @@ void insert(node **root, node *new_node)
 {
     size_t size = get_size(new_node);
 
+	// if tree is empty, sets node to be inserted to be root
     if (*root == 0)
     {
         *root = new_node;
@@ -235,6 +253,7 @@ void insert(node **root, node *new_node)
         }
 
         new_node->parent = prev_node;
+
         // if (get_size(prev_node) < size) {
         if (compare(prev_node, new_node))
         {
@@ -245,11 +264,12 @@ void insert(node **root, node *new_node)
             prev_node->left = new_node;
         }
 
+		// once inserted, have to fix coloring conflicts
         fix(root, new_node);
     }
 }
-/////DELETE is below
-////
+
+// ~~~~~~~~~~~ DELETION BEGINS HERE ~~~~~~~~~~~~~
 node *sibling(node *k)
 {
     if (k->parent == 0)
@@ -484,115 +504,122 @@ void delete (node **root, node *new_node)
     delete (root, repl);
 }
 
-void make_node(void *location, node *left, node *right, node *parent) {
-		node *cur_loc = (node*) location;
-		node cur_node = {parent, left, right};
-		*cur_loc = cur_node;
-}
-
-void make_meta(void *location, size_t size, int is_black, int is_used) { 
-		meta *cur_loc = (meta*) location;
-		meta cur_meta = {size, is_black, is_used};
-		*cur_loc = cur_meta;
-}
-
 // returns a node of at least size_t size from list
 node *search(size_t size, node *list) {
+		// if tree is empty, no valid node
 		if (list == 0) return 0;
 
 		node *runner = list;
 		node *prev_node = 0;
 
+		// while runner isn't node and runner isn't perfect node
 		while (runner != 0 && get_size(runner) != size) {
 				prev_node = runner;
 				runner = get_size(runner) < size ? runner->right : runner->left;
 		}
 
+		// if runner is 0, backtrace upwards from prev_node until valid node or no parents left
 		if (runner == 0) {
 				while (prev_node != 0 && get_size(prev_node) < size) {
 						prev_node = prev_node->parent;
 				}						
 				return prev_node; 
 		}
+		// if runner is prefect node, return runner
 		else {
 				return runner;	
 		}
-
-		/*
-		if (runner == 0) {
-				if (get_size(prev_node) >= size) return prev_node;
-				else return 0;
-		}
-		else if (get_size(runner) == size) return runner;
-		else if (get_size(runner) > size) return runner->parent;
-		else return 0;
-		*/
 }
 
+// initializes the_heap and places it in the tree
 void init_free_list() {
 		// making headers
 		make_meta(&the_heap, HEAP_SIZE - 16, 1, 0);
 		make_meta(&the_heap[LENGTH - 1], HEAP_SIZE - 16, 1, 0);
+
 		// adding to red black tree
 		make_node(&the_heap[1], 0, 0, 0);
+
 		// insert(&free_list, (node*) &the_heap[1]);
 		*free_list = (node*) &the_heap[1];
 } 
 
+// utility function that sets header and footer of a node
 void set_headers(node *cur_node, size_t size, int is_used) {
 		void *start = (void*) cur_node;
 		make_meta(start - 8, size, 0, is_used);
 		make_meta(start + size, size, 0, is_used);	
 }
 
+// returns pointer to block of at least size free space
 void *malloc(size_t size) {
 		mCount++;
+
+		// initialize free list once 
 		if (!has_init) {
 				init_free_list();
 				has_init = !has_init;
 		}
 
 		if (size == 0) return 0;
+
+		// make size multiple of 8 to make byte addressable 
 		size = size + (8 - size % 8) % 8;
 		size = size <= 24 ? 24 : size;
 
+		// find smallest node that is >= size
 		node *valid_node = search(size, *free_list);
+
+		// if exists node that fits size
 		if (valid_node) {
 				delete(free_list, valid_node);
 				size_t cur_size = get_size(valid_node);
+
 				// split if splittable
 				if (cur_size - size >= 40) {
 						// resets header size
 						set_headers(valid_node, size, 1);
 						void *split_block = ((void*) valid_node) + size + 16;
+						// splits block by making new node
 						make_node(split_block, 0, 0, 0);
+						// sets size of headers of new node 
 						set_headers((node*) split_block, cur_size - size - 16, 0);
-						// reinsert if splitted
+
+						// reinsert splitted node 
 						insert(free_list, ((node*) split_block));
 				}
 				else {
 						set_is_used(valid_node, 1);
 				}
+
 				// return valid_node
 				return valid_node;
 		}
+		// no node large enough for size
 		else {
 				return 0;
 		}
 }
 
+// checks if block after cur_node is used
+// used for merging
 int check_post_empty(node *cur_node) {
 		size_t size = get_size(cur_node);
+		// footer of current node
 		meta *cur_footer = (meta*) (((void*) cur_node) +size);
+		
+		// if current block is at end of the_heap, no following block, so return 0
 		if (((void*) cur_footer) == ((void*) the_heap[LENGTH - 1])) {
 				return 0;
 		}
 		else {
+				// return if next block is used
 				meta *next_footer = (meta*) (cur_footer + 1);
 				return next_footer->is_used;
 		}
 }
 
+// same as check_post_empty
 int check_prev_empty(node *cur_node) {
 		if (((void*) cur_node) == ((void*) &the_heap[1])) {
 				return 0;
@@ -603,33 +630,50 @@ int check_prev_empty(node *cur_node) {
 		}
 }
 
+// merges first_node and second_node
+// assumed that first_node comes before second_node
 void merge(node *first_node, node *second_node) {
 		delete(free_list, first_node);
 		delete(free_list, second_node);
 
+		// calculates size of resulting merged block
 		size_t total_size = get_size(first_node) + get_size(second_node);
 		set_headers(first_node, total_size + 16, 0);
 		
+		// insert node in tree and mark as unused 
 		insert(free_list, first_node);
+		set_is_used(first_node, 0);
 }
 
+// marks memory at ptr as free by inserting back into free list
+// merges current block with prior and following block if neccessary
 void free(void *ptr) {		
 		fCount++;
+		
 		node *cur_node = (node*) ptr;
 		int is_insert = 0;
+		
+		// if following block is free
 		if (!check_post_empty(cur_node)) {
+				// node of following block
 				node *next_node = (node*) (ptr + get_size(cur_node) + 16);
+				// merge two nodes
 				merge(cur_node, next_node);
 				is_insert = 1;
 		}
 
+		// if previous block is free
 		if (!check_prev_empty(cur_node)) {
+				// footer of previous block
 				meta *prev_footer = (meta*) (ptr - 16);
+				// gets node of previous block
 				node *prev_node = (node*) (ptr - 16 - prev_footer->size);
+				// merges two nodes
 				merge(prev_node, cur_node);
 				is_insert = 1;
 		}
 
+		// if no merges occur, simply insert current node in tree 
 		if (!is_insert) {
 				insert(free_list, cur_node);
 		}
